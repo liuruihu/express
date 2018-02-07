b28f6fa01d8c91a021a23788631cf32c8147e85e
dell-PC\dell <liuruihu@qifadai.com>
2018-02-07 16:50:05 +0800

    '局部文件在首页没显示，但是在404页面显示出来了，什么情况'

--- meadowlark.js
+++ meadowlark.js
@@ -12,11 +12,15 @@ var fortune = require('./library/fortune.js');
 var handlebars = require('express3-handlebars')
                 .create({
                     defaultLayout:'main'
+                    // ,extname:'.hbs'//这里是改文件名后缀的，除了.hbs，不知道可不可以自定义后缀名
                 });
 
 app.engine('handlebars',handlebars.engine);
-app.set('views','./views/layouts');//这个书上没写，可能指的是路径
 app.set('view engine','handlebars');
+app.set('views','./views/layouts');//这个书上没写，可能指的是路径
+
+
+// app.set('view cache', true);//没看出来有什么缓存的作用
 /*结束
 *设置handlebars视图引擎
 */
@@ -41,6 +45,7 @@ app.use(express.static(__dirname +'/public'));
     app.get('/',function(req,res){
         // res.type('text/plain');
         // res.send('Meadowlark Travel,首页');
+
         res.render('home');
     });
 
@@ -74,6 +79,62 @@ app.use(express.static(__dirname +'/public'));
 
     });
 
+    //foo页面
+    app.get('/foo',function(req,res){
+        res.render('foo',{
+            layout:null
+        });
+    });
+
+
+    //使用其他模板
+    app.get('/other',function(req,res){
+        res.render('other',{
+            layout:'otherLayout'
+        });
+    });
+
+
+    ////////////////////////////
+    //给组件传值
+    function getWeatherData(){
+        return {
+            locations:[
+                {
+                    name:'Portland',
+                    forecastUrl:'http://www.wunderground.com/US/OR/Portland.html',
+                    iconUrl:'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                    weather:'Overcast',
+                    temp:'54.1 F(12.3 C)'
+                },
+                {
+                    name:'Bend',
+                    forecastUrl:'http://www.wunderground.com/US/OR/Bend.html',
+                    iconUrl:'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                    weather:'Partly Cloudy',
+                    temp:'54.1 F(12.3 C)'
+                },
+                {
+                    name:'Manzanita',
+                    forecastUrl:'http://www.wunderground.com/US/OR/Manzanita.html',
+                    iconUrl:'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                    weather:'Light Rain',
+                    temp:'54.1 F(12.3 C)'
+                }
+            ]
+        }
+    }
+    //组件里传值
+    app.use(function(req, res, next){
+        if(!res.locals.partials) res.locals.partials = {};
+        res.locals.partials.weather = getWeatherData();
+        /*
+        *上面两行代码无效，不知道是不是res.locals在app.use的原因
+        *在首页没显示处理数据，但是在404页面显示了，什么情况
+        */
+
+        next();
+    });
 
 
     //定制404页面
